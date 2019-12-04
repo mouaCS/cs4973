b053d68f3942abcd1ed4d88a18a57a41abdfbeae
mouaCS <31530087+mouaCS@users.noreply.github.com>
2019-12-04 12:23:33 -0600

    reduced some lines

--- ProjectServer.c
+++ ProjectServer.c
@@ -3,12 +3,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <errno.h>
 
 // Set Port
 #define PORT 2000
